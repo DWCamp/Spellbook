2bb1cc2c83d862577a8412f880cca5772c047398
Daniel Campman <dwc1280@rit.edu>
2020-03-31 02:23:17 -0400

    Prepare for SpellList re-serialization
    - SpellFE.java absorbed Spell.java and was renamed to just Spell.java
    - FESpellList.txt was converted to UTF-8 to prevent encoding issues on macOS

--- src/model/Spell.java
+++ src/model/Spell.java
@@ -1,50 +1,220 @@
 package model;
 
-import java.io.Serializable;
+public class Spell implements Comparable<Spell>{
+    private String name;
+    private String[] classes;
+    private String school;
+    private int level;
+    private String castingTime;
+    private String range;
+    private String components;
+    private String duration;
+    private String desc;
 
-/**
- * Represents a generic spell of any RPG game edition
- *
- * @author Daniel Campman
- */
-public abstract class Spell implements Serializable {
-	protected String name;
+    /**
+     * Constructor for a spell object
+     * @param name The name of the spell
+     * @param classes The classes the spell belongs to
+     * @param school The wizard school of the spell
+     * @param castingTime The casting time for the spell
+     * @param range The range of the spell
+     * @param components The components of the spell
+     * @param duration The duration of the spell
+     * @param desc The description of the spell
+     */
+    public Spell(String name, String[] classes, String school,
+                 int level, String castingTime, String range,
+                 String components, String duration, String desc)
+    {
+        this.name = name;
+        this.classes = classes;
+        this.school = school;
+        this.level = level;
+        this.castingTime = castingTime;
+        this.range = range;
+        this.components = components;
+        this.duration = duration;
+        this.desc = desc;
+    }
 
-	/**
-	 * Gets the name of the spell
-	 * @return The name of the spell
-	 */
-	public String getName() {
-		return name;
-	}
-	
-	public abstract String getCardText();
-	public abstract int getLevel();
-	public abstract String getSubtitle();
-	public abstract String getPopUpText();
-	public abstract String getDescription();
-	public abstract String getDetails();
+    /**
+     * Gets the name of the spell
+     * @return {@code String}
+     */
+    public String getName()
+    {
+        return name;
+    }
 
-	/**
-	 * Checks if spells are equal
-	 * Two spells are considered equal if they possess the same name
-	 *
-	 * @param other The other object being compared
-	 * @return `true` if the spells are equal
-	 */
-	@Override
-	public boolean equals(Object other) {
-		if (other == null) {
-			return false;
-		}
-		if (other == this) {
-			return true;
-		}
-		if (other instanceof Spell) {
-			Spell otherSpell = (Spell) other;
-			return otherSpell.getName().equals(name);
-		}
+    /**
+     * Gets a String array of all of the
+     * classes that this spell belongs to
+     * @return {@code String[]}
+     */
+    public String[] getClasses()
+    {
+        return classes;
+    }
 
-		return false;
-	}
+    public String getSchool()
+    {
+        return school;
+    }
+
+    /**
+     * Returns the level of the spell as a string (e.g. '1st-level')
+     * @return {@code String}
+     */
+    public String getLevelString()
+    {
+        if (level == 0) {
+            return "Cantrip";
+        } else if (level == 1) {
+            return "1st-level";
+        } else if (level == 2) {
+            return "2nd-level";
+        } else if (level == 3) {
+            return "3rd-level";
+        }
+        return level + "th-level";
+    }
+
+    /**
+     * Returns the level of the spell
+     * @return {@code int} level
+     */
+    public int getLevel()
+    {
+        return level;
+    }
+
+    /**
+     * Returns the Casting Time of the spell
+     * @return {@code String}
+     */
+    public String getCastingTime()
+    {
+        return castingTime;
+    }
+
+    /**
+     * Returns the range of the spell
+     * @return {@code String}
+     */
+    public String getRange()
+    {
+        return range;
+    }
+
+    /**
+     * Returns the components of the spell
+     * @return {@code String}
+     */
+    public String getComponents()
+    {
+        return components;
+    }
+
+    /**
+     * Returns the duration of the spell
+     * @return {@code String}
+     */
+    public String getDuration()
+    {
+        return duration;
+    }
+
+    /**
+     * Returns the description of the spell, including it's <br>
+     * type, casting time, range, components, duration, <br>
+     * and effect
+     * @return {@code String}
+     */
+    public String getDescription()
+    {
+        return desc;
+    }
+
+    /**
+     * Returns a string representation of the object
+     * @return {@code String}
+     */
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+
+    /**
+     * Determines if two spells are equal based on their names
+     * @param spell Other spell
+     * @return boolean Returns {@code true} if they are equal
+     */
+    public boolean equals(Spell spell)
+    {
+        return spell.getName().equals(name);
+    }
+
+    /**
+     * Provides a quick description of the spell, including its level and school
+     * @return The level as a String
+     */
+    public String getSubtitle()
+    {
+        if (level == 0) {
+            return school + " cantrip";
+        }
+        return getLevelString() + " " + school;
+    }
+
+    @Override
+    public int compareTo(Spell other) {
+        return name.toLowerCase().compareTo(other.getName().toLowerCase());
+    }
+
+
+    public String getCardText() {
+        return "Casting Time: " + castingTime
+                + "\n\nRange: " + range
+                + "\n\nComponents: " + components
+                + "\n\nDuration: " + duration;
+    }
+
+
+    public String getPopUpText()
+    {
+        return name + "\n" + getLevelString() + "\n\nCasting time: " + castingTime
+                + "\n\nRange: " + range + "\n\nComponents: " + components
+                + "\n\nDuration: " + duration + "\n\n" + desc;
+    }
+
+
+    public String getDetails() {
+        return getLevelString() + "\n\nCasting time: " + castingTime
+                + "\n\nRange: " + range + "\n\nComponents: " + components
+                + "\n\nDuration: " + duration + "\n\n" + desc;
+    }
+
+    /**
+     * Checks if spells are equal
+     * Two spells are considered equal if they possess the same name
+     *
+     * @param other The other object being compared
+     * @return `true` if the spells are equal
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (other == this) {
+            return true;
+        }
+        if (other instanceof Spell) {
+            Spell otherSpell = (Spell) other;
+            return otherSpell.getName().equals(name);
+        }
+
+        return false;
+    }
 }
